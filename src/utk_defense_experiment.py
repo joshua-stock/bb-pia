14d2f63c3630480a0bcb671dfd0bb0aa359c48d2
Joshua Stock <joshua.stock@uni-hamburg.de>
2024-04-05 09:08:56 +0200

    update utk defense experiment

--- src/utk_defense_experiment.py
+++ src/utk_defense_experiment.py
@@ -8,18 +8,12 @@ from utk_functions import get_lbfw_dataset, get_distributed_utk_sets
 from datetime import datetime
 from common.functions import get_defending_lucasnet_model, compile_categorical_model, ensure_path_exists
 
-
-affinity_mask = set()
-for i in range(10):
-    affinity_mask.add(i)
-os.sched_setaffinity(0, affinity_mask)
-
 resultspath = "utkface/results/"
 ensure_path_exists(resultspath)
-adversary = keras.models.load_model('utkface/models/adv_v1_0.75_r2.keras')
+adversary = keras.models.load_model('utkface/models/adv_v1_0.76_r2.keras')
 model_input = get_lbfw_dataset()
 #distributed_datasets = get_distributed_utk_sets()
-lambdas = [0.05, 0.1, 0.15, 0.2, 0.25, 0.0]
+lambdas = [0.0, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2]
 runs = 10
 time = datetime.now().strftime('%Y%m%d-%H%M%S')
 resultsfile = f"{resultspath}utk_defense_results-{time}.csv"
@@ -33,15 +27,13 @@ def set_seeds(training_lambda, run, distribution):
     tf.random.set_seed(seed)
 
 
-for dis in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-    ds = get_distributed_utk_sets(distributions=[dis])[0]
-    for training_lambda in lambdas:
+
+for training_lambda in lambdas:
+    for dis in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
         for run in range(runs):
+            ds = get_distributed_utk_sets([dis])[0]
             save_path = f"utkface/models/defense/"
             ensure_path_exists(save_path)
-            model_save_path = f"{save_path}utkdef-ds{ds.distribution}-l{training_lambda}-run{run}.keras"
-            if os.path.exists(model_save_path):
-                continue
             logs = f"logs/utkdef-ds{ds.distribution}-l{training_lambda}-run{run}-{time}"
             print(f"Now training ds{ds.distribution}-l{training_lambda}-run{run}-{time}")
             tboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logs,
@@ -66,13 +58,13 @@ for dis in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
                 batch_size=32,
                 callbacks=[tboard_callback],
                 verbose=0)
-            model.save_inner_model(model_save_path)
+            model.save_inner_model(f"{save_path}utkdef-ds{ds.distribution}-l{training_lambda}-run{run}.keras")
 
             output = model.predict(model_input)
             formatted_input = output[:, 0].reshape(1, output.shape[0])
             adv_out = adversary(formatted_input).numpy().flatten()[0]
-            test_acc = model.evaluate(ds.X_train, ds.y_train)
+            test_acc = model.evaluate(ds.X_train, ds.y_train)[2]
 
             with open(resultsfile, 'a', newline='') as csvfile:
                 resultwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                resultwriter.writerow([training_lambda, ds.distribution, run, str(test_acc), adv_out])
+                resultwriter.writerow([training_lambda, ds.distribution, run, test_acc, adv_out])
